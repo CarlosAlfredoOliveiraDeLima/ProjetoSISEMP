22d0ccf6f4a433678ef9b4570f3b1b2d0f4790e9
Carolina Lemes Nascimento Costa <carolinalncosta@ibm.com>
2022-05-01 14:51:51 -0300

    Inserção de htmls
    Inclusão de htmls feito pelo Rafael no projeto.

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-# o ponto é pq estou pegando algo dentro do diretorio do projeto
-from .views import PaginaInicial, SobreView
 
+# Importa as views que a gente criou 
+from .views import PaginaInicial, SobreView, CadastraAtendimento, CadastraPessoa, ConsultaPessoa, ConsultaAtendimento
+
+# Tem que ser urlpatterns porque é padrão do Django
 urlpatterns = [
-    #path('endereco/', MinhaView.as_view(), name='nome-da-url'),
-    path('', PaginaInicial.as_view(), name = 'inicio'),
-    path('sobre/', SobreView.as_view(), name = 'sobre'),
+    # Todo path tem endereço, sua_view.as_view() e nome
+    #path('', PaginaInicial.as_view(), name='index'),
+    path('', PaginaInicial.as_view(), name='home'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
+    path('cadastraAtendimento/', CadastraAtendimento.as_view(), name='cadastraAtendimento'),
+    path('cadastraPessoa/', CadastraPessoa.as_view(), name='cadastraPessoa'),
+    path('consultaPessoa/', ConsultaPessoa.as_view(), name='consultaPessoa'),
+    path('consultaAtendimento/', ConsultaPessoa.as_view(), name='consultaAtendimento'),
 ]
